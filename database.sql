230150f2f327d91a670912129cdd6afd3623835b
gigizhong5 <70976322+gigizhong5@users.noreply.github.com>
2022-03-31 20:23:37 -0500

    database

---
+++ database.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE fuelapp;
+
+CREATE TABLE usercredentials(
+    username VARCHAR PRIMARY KEY,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE clientinformation(
+    username VARCHAR PRIMARY KEY,
+    full_name VARCHAR(50) NOT NULL,
+    address_1 VARCHAR(100) NOT NULL,
+    address_2 VARCHAR(100),
+    city VARCHAR(100) NOT NULL,
+    state CHAR(2) NOT NULL,
+    zip_code VARCHAR(9) CHECK (length(zip_code) > 4) NOT NULL
+);
+
+CREATE TABLE fuelquote(
+    quote_id SERIAL PRIMARY KEY,
+    username VARCHAR,
+    gallons_requested NUMERIC,
+    delivery_address VARCHAR,
+    delivery_date VARCHAR,
+    suggested_price NUMERIC(15,2),
+    total_amount NUMERIC(15,2)
+);
