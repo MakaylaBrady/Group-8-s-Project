413beefaead19dbcb166e03d415893dbc1ff58f9
MWalMalik <48228703+MWalMalik@users.noreply.github.com>
2022-05-04 11:36:05 -0500

    Delete public directory

--- public/js/setMinDate.js
+++
@@ -1,6 +0,0 @@
-const dateElement = document.querySelector('#deliveryDate');
-
-let currentDate = new Date();
-
-currentDate.setDate(currentDate.getDate() + 1);
-dateElement.setAttribute('min', currentDate.toISOString().split('T')[0]);
